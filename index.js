7d699edcc38158384ccf76aed0f099c2fe33f4b3
A Ram Pandey <shashwatpandey100@gmail.com>
2024-10-17 21:18:18 +0530

    trying to deploy by setting HOST in app.listen to '0.0.0.0'

--- index.js
+++ index.js
@@ -15,9 +15,10 @@ const loaders = require("@medusajs/medusa/dist/loaders/index").default
       })
       const configModule = container.resolve("configModule")
       const port = process.env.PORT || 9000
+      const HOST = '0.0.0.0';
 
       const server = GracefulShutdownServer.create(
-        app.listen(port, (err) => {
+        app.listen(port,  HOST, (err) => {
           if (err) {
             return
           }
