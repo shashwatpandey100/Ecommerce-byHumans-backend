a9501782d72cb8eb113e98f511fedf3cb47b2e9c
A Ram Pandey <shashwatpandey100@gmail.com>
2024-10-17 18:06:46 +0530

    trying to deploy with medusajs/admin ka autoRebuild: false becuase of no port issue

--- medusa-config.js
+++ medusa-config.js
@@ -73,7 +73,7 @@ const plugins = [
     resolve: "@medusajs/admin",
     /** @type {import('@medusajs/admin').PluginOptions} */
     options: {
-      autoRebuild: true,
+      autoRebuild: false,
       develop: {
         open: process.env.OPEN_BROWSER !== "false",
       },
